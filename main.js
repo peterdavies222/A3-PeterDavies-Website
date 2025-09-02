946207262cfe3afd70dc9d83256e2e8824b575b5
peterdavies222 <peterdavies222@gmail.com>
2025-09-02 16:11:14 +0800

    trying to fix stockists -1

--- main.js
+++ main.js
@@ -63,23 +63,24 @@ const scrollers = document.querySelectorAll('.scroller')
 if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
     addAnimation()
 }
-
-function addAnimation() {
-    scrollers.forEach(scroller => {
-        scroller.setAttribute('data-animated', true)
-
-        const scrollerInner = scroller.querySelector('.scroller__inner')
-        const scrollerContent = Array.from(scrollerInner.children)
-
-        for (let i = 0; i < 2; i++) {
-            scrollerContent.forEach(item => {
-                const duplicatedItem = item.cloneNode(true)
-                duplicatedItem.setAttribute("aria-hidden", true)
-                scrollerInner.appendChild(duplicatedItem)
-            })
-        }
-    })
-}
+document.addEventListener("DOMContentLoaded", ()=> {
+    function addAnimation() {
+        scrollers.forEach(scroller => {
+            scroller.setAttribute('data-animated', true)
+    
+            const scrollerInner = scroller.querySelector('.scroller__inner')
+            const scrollerContent = Array.from(scrollerInner.children)
+    
+            for (let i = 0; i < 2; i++) {
+                scrollerContent.forEach(item => {
+                    const duplicatedItem = item.cloneNode(true)
+                    duplicatedItem.setAttribute("aria-hidden", true)
+                    scrollerInner.appendChild(duplicatedItem)
+                })
+            }
+        })
+    }
+})
 
 const products = document.querySelector(".products--full")
 const productsScrollRight = document.getElementById("products-scrollright")
